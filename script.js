8016a8c0643cb929339cf6c5369f269c3601a220
Ananda Fitra <anandadf@student.ub.ac.id>
2023-04-19 08:36:17 +0700

    Added update DOM logic, several adjustment

--- script.js
+++ script.js
@@ -1,18 +1,68 @@
 let rows = document.querySelectorAll(".row");
+let activeEditElement = null;
+let editSection = document.querySelector(".edit-container");
+let oldData = { "no": null, "nim": null, "nama": null, "prodi": null, "email": null };
 
 rows.forEach((el) => {
+    let updateBtn = el.querySelector('.update'),
+        deleteBtn = el.querySelector('.delete');
+
     // Attempt updating when clicked 
-    el.querySelector('.update').addEventListener('click', () => {
+    updateBtn.addEventListener('click', () => {
         console.log("update triggered!");
 
+        if (activeEditElement != null) {
+            // Set the default state
+            activeEditElement.querySelector('.update').disabled = false;
+            activeEditElement.querySelector('.delete').disabled = false;
+        }
+        activeEditElement = el;
+
+        // show edit section
+        editSection.style = '';
+
+        // Disable button when editing
+        updateBtn.disabled = true;
+        deleteBtn.disabled = true;
+
+        let editInputs = editSection.querySelectorAll('input[type=text]');
+
         // Save previous data before any updates
-        oldData = { "no": null, "nim": null, "nama": null, "program studi": null, "email": null };
         Object.keys(oldData).forEach((col, j) => {
             oldData[col] = el.children[j].textContent;
-        })
+            if (j > 0) {
+                editInputs[j-1].value = oldData[col];
+            }
+        });
+
+        const resetState = () => {
+            editSection.querySelector('#update-confirm').removeEventListener('click', goUpdate);
+            editSection.querySelector('#cancel-confirm').removeEventListener('click', goCancel);
+            activeEditElement.querySelector('.update').disabled = false;
+            activeEditElement.querySelector('.delete').disabled = false;
+
+            // hide back edit section
+            editSection.style = 'display: none';
+        }
+    
+
+        const goUpdate = () => {
+            editInputs.forEach((inputs, j) => {
+                console.log(inputs);
+                el.children[j+1].textContent = inputs.value;
+            });
+            resetState();
+        };
+        
+        const goCancel = () => {
+            resetState();
+        };
+
+        editSection.querySelector('#update-confirm').addEventListener('click', goUpdate);
+        editSection.querySelector('#cancel-confirm').addEventListener('click', goCancel);
     });
 
-    el.querySelector('.delete').addEventListener('click', () => {
+    deleteBtn.addEventListener('click', () => {
         el.remove();
 
         // refresh new row children
--- v_data-mhs.php
+++ v_data-mhs.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <title>Data Mahasiswa</title>
-    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <header>
         <h2>Data Mahasiswa</h2>
     </header>
     <div class="container">
-        <h4>Halo, 
-        <?php 
+        <h4>Halo,
+            <?php
             if ($isAdmin) {
                 echo 'Admin</h4>';
                 echo '<a href=".?logout=1">Log Out</a>';
@@ -20,37 +21,60 @@
                 echo '<a href="login.php">Log In</a>';
                 echo '<br></br>';
             }
-        ?>
-        <table>
-            <tr>
-                <th>No</th>
-                <th>NIM</th>
-                <th>Nama</th>
-                <th>Program Studi</th>
-                <th>Email</th>
-                <?php 
-                    if ($isAdmin) {
-                        echo '<th>Aksi</th>';
-                    }
-                ?>
-            </tr>
-            <?php 
-            for ($row = 1; $row <= 10; $row++) {
-                echo '<tr class="row">';
-                for ($col = 1; $col <= 5; $col++) {
-                    echo '<td>' . (($col > 1) ? "Data $row, $col" : $row) . '</td>';
-                }
-                if ($isAdmin) { ?>
-                <td>
-                    <button class="action-btn update">Update</button>
-                    <button class="action-btn delete">Hapus</button>
-                </td>
-                <?php }
-                echo '</tr>';
-            }
             ?>
-        </table>
+            <div class="inner">
+                <table>
+                    <tr>
+                        <th>No</th>
+                        <th>NIM</th>
+                        <th>Nama</th>
+                        <th>Program Studi</th>
+                        <th>Email</th>
+                        <?php
+                        if ($isAdmin) {
+                            echo '<th>Aksi</th>';
+                        }
+                        ?>
+                    </tr>
+                    <?php
+                    for ($row = 1; $row <= 10; $row++) {
+                        echo '<tr class="row">';
+                        for ($col = 1; $col <= 5; $col++) {
+                            echo '<td>' . (($col > 1) ? "Data $row, $col" : $row) . '</td>';
+                        }
+                        if ($isAdmin) { ?>
+                            <td>
+                                <button class="action-btn update">Update</button>
+                                <button class="action-btn delete">Hapus</button>
+                            </td>
+                    <?php }
+                        echo '</tr>';
+                    }
+                    ?>
+                </table>
+                <?php 
+                if ($isAdmin) {
+                ?>
+                <div class="edit-container" style="display: none">
+                    <h3>Ubah Data Mahasiswa</h3>
+                        <span>NIM</span>
+                        <input type="text" class="textfield" name="nim" id="nim">
+                        <span>Nama</span>
+                        <input type="text" class="textfield" name="nama" id="nama">
+                        <span>Program Studi</span>
+                        <input type="text" class="textfield" name="prodi" id="prodi">
+                        <span>Email</span>
+                        <input type="text" class="textfield" name="email" id="email">
+                        <div id="edit-btns">
+                            <input type="button" id="update-confirm" value="Ubah">
+                            <input type="button" id="cancel-confirm" value="Batal">
+                        </div>
+                </div>
+            </div>
+            <?php } ?>
     </div>
+    <link rel="stylesheet" href="style.css">
     <script src="script.js"></script>
 </body>
+
 </html>
